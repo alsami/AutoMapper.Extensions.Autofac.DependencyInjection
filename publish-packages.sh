d45bcad6107fd2d0a396b1f80782eac21a1bff20
alsami <al.khatib.sami1990@gmail.com>
2019-03-24 19:16:57 +0100

    update publish-script

--- publish-packages.sh
+++ publish-packages.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-cd src/AutoMapper.Extensions.Autofac.DependencyInjection
-dotnet pack AutoMapper.Extensions.Autofac.DependencyInjection.csproj --include-symbols -c Release --output "."
-dotnet nuget push -s ${1} -k ${2} "CleanCodeLabs.AutoMapper.Extensions.Autofac.DependencyInjection.${3}.symbols.nupkg"
-if [[ ${?} != 0  ]]
-then
-    exit -1
-fi
-cd ..
+cd src
+for directory in *; do 
+    if [[ -d ${directory} ]]; then
+        cd ${directory}
+        dotnet pack *.csproj --include-symbols -c Release --output "."
+        dotnet nuget push -s ${1} -k ${2} "CleanCodeLabs.${directory}.${3}.symbols.nupkg"
+        if [[ ${?} != 0  ]]
+        then
+            exit -1
+        fi
+        cd ..
+    fi
+done
